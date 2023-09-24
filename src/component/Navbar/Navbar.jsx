307f1c1a08298cfa8c1681266d6718e61134d439
Sahana Koli <sahanasultanakoli@gmail.com>
2023-09-24 22:56:38 +0600

    setup navbar layout

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,23 +1,37 @@
-import { Link } from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div>
-            <nav className=" flex justify-between items-center px-6">
+            <nav className=" flex justify-between items-center px-10 mt-8">
                 <div>
                     <img src="https://i.ibb.co/PZ1q8Qw/Logo.png" alt="" />
                 </div>
-                <ul className=" flex gap-5">
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/donation">Donation</Link>
-                    </li>
-                    <li>
-                        <Link to="/statistics">Statistics</Link>
-                    </li>
+                <ul className=" flex gap-10 underline">
+                <NavLink
+                   to="/"
+                   className={({ isActive, isPending }) =>
+                   isPending ? "pending" : isActive ? "text-[#FF444A]" : "active"
+                   }
+                    >
+                   Home
+                  </NavLink>
+                <NavLink
+                   to="/donation"
+                   className={({ isActive, isPending }) =>
+                   isPending ? "pending" : isActive ? "text-[#FF444A]" : "active"
+                   }
+                    >
+                   Donation
+                  </NavLink>
+                <NavLink
+                   to="/statistics"
+                   className={({ isActive, isPending }) =>
+                   isPending ? "pending" : isActive ? "text-[#FF444A]" : "active"
+                   }
+                    >
+                   Statistics
+                  </NavLink>
                 </ul>
             </nav>
         </div>
